66c6dfe2e2c21c729c5ca507f06eec79e5baf808
CSGC6176 <govind36889@gmail.com>
2022-08-25 16:53:35 +0530

    file rename

---
+++ day_1/Level1.py
@@ -0,0 +1,19 @@
+#To check python version type python in terminal
+
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("John"))
+print(type("Doe"))
+print(type("Kenya"))
---
+++ day_1/Level3.py
@@ -0,0 +1,17 @@
+from cmath import sqrt
+
+
+age=18
+time=1.01
+complex_no=2+3j
+name='John'
+male=True
+ls=['China','America']
+tp=(26,34)
+st={3.4,5.7}
+dict={"first_name" :"John" ,"last_name":"Doe"}
+
+diff=((10-2)**2)-((8-3)**2)
+Euc_distance=(diff)**0.5
+print(format((Euc_distance),".2f"))
+
